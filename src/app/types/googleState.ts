d97590b0eb0734d74d44e9b89dc2a8463467a188
Fedor Kobyakov <fedor.kobyakov@onlyoffice.com>
2026-02-26 20:53:30 +0300

    refactor: remove unused IntegrationPannel component and GoogleState type

--- src/app/types/googleState.ts
+++
@@ -1,9 +0,0 @@
-export const GoogleState = {
-  NOT_CONNECTED: "notConnected",
-  HAS_REFRESH_TOKEN: "hasRefreshToken",
-  UPDATING: "updating",
-  CONNECTED: "connected", 
-  ERROR: "error",
-} as const;
-
-export type GoogleState = typeof GoogleState[keyof typeof GoogleState];
